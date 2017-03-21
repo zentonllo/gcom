34a257416e5cc0a39b8e7bcb15ec05085073ea9b
zentonllo <atercenor@gmail.com>
2017-03-21 16:17:24 +0100

    Añadido benchmark en el problema de la espiral

--- pr2/test_classify_plane_points.py
+++ pr2/test_classify_plane_points.py
@@ -21,18 +21,18 @@ nb_black = 50
 nb_red = 50
 nb_data = nb_black + nb_red
 
-s = np.linspace(0, 2*np.pi, nb_black)
+s = np.linspace(0, 2 * np.pi, nb_black)
 
 x_black = np.vstack([np.cos(s), np.sin(s)]).T +\
-          np.random.randn(nb_black, 2) * 0
-x_red = np.vstack([2*np.cos(s), 2*np.sin(s)]).T +\
-        np.random.randn(nb_red, 2) * 0
+    np.random.randn(nb_black, 2) * 0
+x_red = np.vstack([2 * np.cos(s), 2 * np.sin(s)]).T +\
+    np.random.randn(nb_red, 2) * 0
 
 x_middle = (x_black + x_red) / 2
 
 x_data = np.vstack((x_black, x_red))
 
-t_data = np.asarray([0]*nb_black + [1]*nb_red).reshape(nb_data, 1)
+t_data = np.asarray([0] * nb_black + [1] * nb_red).reshape(nb_data, 1)
 
 
 #  Net structure
@@ -70,7 +70,7 @@ mlp = MLP(K_list,
 mlp.train(x_data, t_data,
           epochs=1000, batch_size=10,
           epsilon=0.1,
-          beta =0.001,
+          beta=0.001,
           print_cost=True)
 
 time_end = time.time()
@@ -82,19 +82,19 @@ print('Time used in training %f' % (time_end - time_begin))
 # correctly classified
 
 dr = 0.1
-r1 = 3/2 - dr
-r2 = 3/2 + dr
+r1 = 3 / 2 - dr
+r2 = 3 / 2 + dr
 
-s_test = np.linspace(0, 2*np.pi, 100)
+s_test = np.linspace(0, 2 * np.pi, 100)
 black_pts_test = r1 * np.array([np.cos(s_test), np.sin(s_test)]).T
 red_pts_test = r2 * np.array([np.cos(s_test), np.sin(s_test)]).T
 
 mlp.get_activations_and_units(black_pts_test)
-wrong_black = (mlp.y > 1/2).squeeze()
+wrong_black = (mlp.y > 1 / 2).squeeze()
 print('Points misclassified as black:%f' % np.sum(wrong_black))
 
 mlp.get_activations_and_units(red_pts_test)
-wrong_red = (mlp.y < 1/2).squeeze()
+wrong_red = (mlp.y < 1 / 2).squeeze()
 print('Points misclassified as red:%f' % np.sum(wrong_red))
 
 
--- pr2/test_classify_plane_points_v2.py
+++ pr2/test_classify_plane_points_v2.py
@@ -20,7 +20,7 @@ nb_black = 100
 nb_red = 100
 nb_data = nb_black + nb_red
 
-s = np.linspace(0, 4*np.pi, nb_black)
+s = np.linspace(0, 4 * np.pi, nb_black)
 
 x_black = np.vstack([np.log(1 + s) * np.cos(s),
                      np.log(1 + s) * np.sin(s)]).T
@@ -29,7 +29,7 @@ x_red = np.vstack([-np.log(1 + s) * np.cos(s),
 
 x_data = np.vstack((x_black, x_red))
 
-t_data = np.asarray([0]*nb_black + [1]*nb_red).reshape(nb_data, 1)
+t_data = np.asarray([0] * nb_black + [1] * nb_red).reshape(nb_data, 1)
 
 
 #  Net structure
@@ -63,71 +63,97 @@ diff_activation_functions = [MLP.dtanh,
 
 # network training
 
-time_begin = time.time()
+# List of different learning (epsilons) and regularization (betas) rates
+# used in this benchmark
+learning_rates = np.linspace(0.1, 0.5, 10)
+regularization_rates = np.linspace(0.05, 0.1, 10)
 
-mlp = MLP(K_list,
-          activation_functions, diff_activation_functions)
+# We will only accept results under this threshold
+min_wrongs = 200
 
+# Iterate over the different rates
+for epsilon_test in learning_rates:
+    for beta_test in regularization_rates:
+        time_begin = time.time()
+
+        mlp = MLP(K_list,
+                  activation_functions, diff_activation_functions)
+
+        mlp.train(x_data, t_data,
+                  epochs=2000, batch_size=10,
+                  epsilon=epsilon_test,
+                  beta=beta_test,
+                  print_cost=False)
+
+        time_end = time.time()
+
+        # check if circles nearby the middle one are
+        # correctly classified
+
+        mlp.get_activations_and_units(x_black)
+        wrong_black = (mlp.y > 1 / 2).squeeze()
+
+        mlp.get_activations_and_units(x_red)
+        wrong_red = (mlp.y < 1 / 2).squeeze()
+
+        miss_red = np.sum(wrong_red)
+        miss_black = np.sum(wrong_black)
+        missclassified = miss_red + miss_black
+        
+        #  Prepare plots and save current best parameters
+        if (missclassified < min_wrongs):
+            min_wrongs = missclassified
+            min_wrong_black = miss_red
+            min_wrong_red = miss_black
+            best_epsilon = epsilon_test
+            best_beta = beta_test
+            time_best_train = time_end - time_begin
+            # plot the probability mapping and the data
+
+            delta = 0.01
+            x = np.arange(-3, 3, delta)
+            y = np.arange(-3, 3, delta)
+
+            X, Y = np.meshgrid(x, y)
+            x_pts = np.vstack((X.flatten(), Y.flatten())).T
+            mlp.get_activations_and_units(x_pts)
+            grid_size = X.shape[0]
+            Z = mlp.y.reshape(grid_size, grid_size)
+
+            plt.axis('equal')
+            plt.contourf(X, Y, Z, 50)
+            plt.scatter(x_black[:, 0], x_black[:, 1],
+                        marker=',',
+                        s=1,
+                        color='black')
+            plt.scatter(x_red[:, 0], x_red[:, 1],
+                        marker=',',
+                        s=1,
+                        color='red')
+
+            plt.scatter(x_black[wrong_black, 0],
+                        x_black[wrong_black, 1],
+                        facecolors='None',
+                        s=50,
+                        marker='o', color='red')
+
+            plt.scatter(x_red[wrong_red, 0],
+                        x_red[wrong_red, 1],
+                        facecolors='None',
+                        s=50,
+                        marker='o', color='black')
+
+
+# We execute the best train performed in order to find out its cost
+mlp = MLP(K_list, activation_functions, diff_activation_functions)
 mlp.train(x_data, t_data,
           epochs=2000, batch_size=10,
-          epsilon=0.1,
-          beta = 0.001,
+          epsilon=best_epsilon,
+          beta=best_beta,
           print_cost=True)
-
-time_end = time.time()
-
-print('Time used in training %f' % (time_end - time_begin))
-
-
-# check if circles nearby the middle one are
-# correctly classified
-
-
-mlp.get_activations_and_units(x_black)
-wrong_black = (mlp.y > 1/2).squeeze()
-print('Points misclassified as black: {}'.format(np.sum(wrong_black)))
-
-mlp.get_activations_and_units(x_red)
-wrong_red = (mlp.y < 1/2).squeeze()
-print('Points misclassified as red: {}'.format(np.sum(wrong_red)))
-
-
-# plot the probability mapping and the data
-
-delta = 0.01
-x = np.arange(-3, 3, delta)
-y = np.arange(-3, 3, delta)
-
-
-X, Y = np.meshgrid(x, y)
-x_pts = np.vstack((X.flatten(), Y.flatten())).T
-mlp.get_activations_and_units(x_pts)
-grid_size = X.shape[0]
-Z = mlp.y.reshape(grid_size, grid_size)
-
-plt.axis('equal')
-plt.contourf(X, Y, Z, 50)
-plt.scatter(x_black[:, 0], x_black[:, 1],
-            marker=',',
-            s=1,
-            color='black')
-plt.scatter(x_red[:, 0], x_red[:, 1],
-            marker=',',
-            s=1,
-            color='red')
-
-
-plt.scatter(x_black[wrong_black, 0],
-            x_black[wrong_black, 1],
-            facecolors='None',
-            s=50,
-            marker='o', color='red')
-
-plt.scatter(x_red[wrong_red, 0],
-            x_red[wrong_red, 1],
-            facecolors='None',
-            s=50,
-            marker='o', color='black')
-
-
+print('Time used in training %f' % time_best_train)
+print('Optimal epsilon parameter: %f' % best_epsilon)
+print('Optimal beta (regularization) parameter: %f' % best_beta)
+print('Points misclassified as red: {}'.format(min_wrong_red))
+print('Points misclassified as black: {}'.format(min_wrong_black))
 plt.show()
