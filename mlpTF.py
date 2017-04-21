c3bb9b62ee91cef4981416a7bc56cc5c4a1d0053
zentonllo <atercenor@gmail.com>
2017-04-21 14:36:12 +0200

    mlp funcionando en TF

--- mlpTF.py
+++ mlpTF.py
@@ -1,90 +1,83 @@
-'''
-A Multilayer Perceptron implementation example using TensorFlow library.
-This example is using the MNIST database of handwritten digits
-(http://yann.lecun.com/exdb/mnist/)
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
 
 from __future__ import print_function
 
+import tensorflow as tf
+import numpy as np
+
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-import tensorflow as tf
-
-# Parameters
-learning_rate = 0.001
-training_epochs = 15
-batch_size = 100
-display_step = 1
-
-# Network Parameters
-n_hidden_1 = 256 # 1st layer number of features
-n_hidden_2 = 256 # 2nd layer number of features
-n_input = 784 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
-
-
-
-
-def fc_layer(z, dim, activ_function):
-    w_shape = (int(z.get_shape()[1]),dim)
-    w_init = tf.truncated_normal(w_shape)
-    w = tf.Variable(w_init, name='weights')
+#  Fully connected layer 
+def fc_layer(z, n_neurons, activ_function, reg_function):
+    n_inputs = int(z.get_shape()[1])
+    stddev = 1 / np.sqrt(n_inputs)
+    w_shape = (n_inputs,n_neurons)
+    w_init = tf.truncated_normal(w_shape, stddev=stddev)
+    w = tf.Variable(w_init, name="weights")
     
-    b_init = tf.truncated_normal(dim)    
+    b_init = tf.zeros([n_neurons])  
     b = tf.Variable(b_init, name='biases')
     
     a = tf.matmul(z, w) + b
-    return activ_function(a)
-
-def multilayer_perceptron(x, y, nb_epochs, K_list, activ_functions):
-
-    # tf Graph input
-    x = tf.placeholder("float", [None, n_input])
-    y = tf.placeholder("float", [None, n_classes])
     
-    for layer in range(nb_epochs):
+    reg_loss = 0
+    if reg_function is not None:
+        reg_loss = reg_function(w)
         
-        
-    
-    
+    return activ_function(a), reg_loss
 
-# Create model
-def multilayer_perceptron(x, weights, biases):
-    # Hidden layer with RELU activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-    # Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-    # Output layer with linear activation
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-    return out_layer
 
-# Store layers weight & bias
-weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
-}
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
-}
+#  Reset del grafo
+tf.reset_default_graph()
 
-# Construct model
-pred = multilayer_perceptron(x, weights, biases)
+# Lectura del dataset
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-# Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+n_input = 784 # MNIST data input (img shape: 28*28)
+n_classes = 10 # MNIST total classes (0-9 digits)
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 300
+batch_size = 100
+display_step = 1
+
+
+
+# Network Parameters
+
+hidden_list = [256, 256, n_classes]
+activ_functions = [tf.nn.relu]*(len(hidden_list)-1) + [tf.identity]
+regularizers = [None]*len(hidden_list)
+#regularizers = [tf.nn.l2_loss]*len(hidden_list)
+dropouts = [0.5,0.5] + [None]
+beta = 0
+
+
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+reg_cost = 0
+reg_loss = 0
+
+nb_hidden = len(hidden_list)
+z = x
+for layer in range(nb_hidden):
+    z, reg_loss = fc_layer(z,hidden_list[layer],activ_functions[layer],regularizers[layer])
+    reg_cost += reg_loss
+    if dropouts[layer] is not None:
+        z = tf.nn.dropout(z, dropouts[layer])
+
+
+out = tf.nn.softmax_cross_entropy_with_logits(logits=z,labels=y)
+cost = tf.reduce_mean(out + beta*reg_cost)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
+
 
 # Launch the graph
 with tf.Session() as sess:
@@ -109,8 +102,8 @@ with tf.Session() as sess:
     print("Optimization Finished!")
 
     # Test model
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    correct_prediction = tf.equal(tf.argmax(z, 1), tf.argmax(y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
